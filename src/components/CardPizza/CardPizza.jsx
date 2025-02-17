c1abcb7a8c90962c60d8d476c5d4d9cb1bfcdf41
muriel-ap <murielavila.p@gmail.com>
2025-02-17 00:06:09 -0300

    update: se reemplaza código de jsx por card de props de Pizza.js

--- src/components/CardPizza/CardPizza.jsx
+++ src/components/CardPizza/CardPizza.jsx
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
 import "./CardPizza.css";
 
-const CardPizza = ({ name, price, ingredients, img }) => {
+const CardPizza = ({ img, name, desc, price, ingredients }) => {
   return (
     <div className="card">
       <img src={img} className="card-img-top" alt={name} />
-      <h2 className="card-title py-2 m-0">{name}</h2>
-      <hr />
-      <p className="card-text m-0">Ingredientes:</p>
-      <p className="card-subtext">🍕{ingredients}</p>
-      <hr />
-      <p className="fs-4 fw-bold">Precio: ${price}</p>
-      <div className="card-footer d-flex justify-content-between">
-        <button className="btn btn-outline-dark">👀Ver más</button>
+      <h3 className="card-title py-2 m-0">{name}</h3>
+      <p className="card-text m-0">{desc}</p>
+      <p className="fw-bold my-2">Ingredientes:</p>
+      <ul className="list-container">
+        {ingredients.map((ingredient) => (
+          <li className="card-subtext" key={ingredient}>
+            🍕{ingredient}.
+          </li>
+        ))}
+      </ul>
+      <p className="card-price fs-4 fw-bold">Precio: ${price}</p>
+      <div className="card-footer">
         <button className="btn btn-dark">🛒Añadir</button>
       </div>
     </div>
@@ -24,6 +28,7 @@ CardPizza.propTypes = {
   price: PropTypes.number.isRequired,
   ingredients: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
 };
 
 export default CardPizza;
