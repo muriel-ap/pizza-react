fc5c8fb4c1ecf4ff70bfb6c46591660c9de1cf70
muriel-ap <murielavila.p@gmail.com>
2025-03-10 00:49:45 -0300

    update: se agrega provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./components/context/CartContext";
 import Navigation from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Pizza from "./pages/Pizza";
 import Register from "./pages/Forms/Register";
 import Login from "./pages/Forms/Login";
-import Cart from "./pages/Cart"
+import Cart from "./pages/Cart";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 
-
-
 function App() {
   return (
-
     <BrowserRouter>
-      <Navigation />
-      <div className="page">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/pizza/p001" element={<Pizza />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<NotFound />} />
-
-
-      </Routes>
-      </div>
-      <Footer />
+      <CartProvider>
+        <Navigation />
+        <div className="page">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/pizza/p001" element={<Pizza />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
+      </CartProvider>
     </BrowserRouter>
-
   );
 }
 
