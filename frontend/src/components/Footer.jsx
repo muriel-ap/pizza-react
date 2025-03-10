204a944c8888e7c0776f9cedba852d2172a824e8
muriel-ap <murielavila.p@gmail.com>
2025-03-09 21:12:59 -0300

    update: se comidifca estilo mediante Bootstrap

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="footer-container bg-dark">
+    <div className="footer bg-dark d-flex justify-content-center align-items-center py-3">
       <p className="text-light m-0">
         © 2025 Pizzería Mama Mía! - Todos los derechos reservados{" "}
       </p>
