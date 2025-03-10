2647846fd897ee73dabbcf45e53a9bcd385cc119
muriel-ap <murielavila.p@gmail.com>
2025-03-10 00:56:32 -0300

    add: mostrar en Navbar el  total de precio de los productos seleccionados en cart

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { useCartCont } from "./context/CartContext";
 
 const Navigation = () => {
+  const { getTotal } = useCartCont();
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg" className="fixed-top">
       <Container>
         <Navbar.Brand as={Link} to="/">
           🍕 Pizzería Mamma Mía
@@ -12,11 +14,17 @@ const Navigation = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/register">🔐Registro</Nav.Link>
-            <Nav.Link as={Link} to="/login">🔐Login</Nav.Link>
-            <Nav.Link as={Link} to="/profile">🔓Perfil</Nav.Link>
-            <Nav.Link as={Link} to="/cart" className="fw-bold text-warning">
-              🛒 Total: $
+            <Nav.Link as={Link} to="/register">
+              🔐Registro
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              🔐Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/profile">
+              🔓Perfil
+            </Nav.Link>
+            <Nav.Link as={Link} to="/cart" className="fw-bold text-info">
+              🛒 Total: ${getTotal()}
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
