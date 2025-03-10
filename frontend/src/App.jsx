16db8df6ce6842ad8b7796b1714fe2e10daca1de
muriel-ap <murielavila.p@gmail.com>
2025-03-09 21:10:33 -0300

    update: se agrega navegación mediante React Browser Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,37 @@
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navbar";
+import Home from "./pages/Home";
 import Footer from "./components/Footer";
-import Pizza from "./components/Pizza";
-// import Register from "./components/Forms/Register";
-// import Login from "./components/Forms/Login";
-//import Cart from "./components/Cart"
+import Pizza from "./pages/Pizza";
+import Register from "./pages/Forms/Register";
+import Login from "./pages/Forms/Login";
+import Cart from "./pages/Cart"
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+
+
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      {/*<Register />*/}
-      {/* <Login /> */}
-      {/*<Cart />*/}
-      <Pizza />
 
+    <BrowserRouter>
+      <Navigation />
+      <div className="page">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/pizza/p001" element={<Pizza />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+
+
+      </Routes>
+      </div>
       <Footer />
-    </>
+    </BrowserRouter>
+
   );
 }
 
