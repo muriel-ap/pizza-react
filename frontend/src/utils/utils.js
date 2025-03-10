92fb81b4b835cde99b0f27cb3f4d88de689e7f1e
muriel-ap <murielavila.p@gmail.com>
2025-03-09 21:20:01 -0300

    update: se elimina función

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -2,8 +2,4 @@ export const formatPrice = (price) => {
   return price.toLocaleString('es-CL');
 };
 
-const capFirst = (string) => {
-  if (typeof string !== "string") return string;
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
 
